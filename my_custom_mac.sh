6f49abc7157807df6d622cfea7f37f9aa5785fdb
luzhangting <luzhangting@meituan.com>
2018-03-21 18:56:35 +0800

    first commit

---
+++ my_custom_mac.sh
@@ -0,0 +1,10 @@
+# 安装brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# 使用brew安装软件，brew cask能一次安装好多软件~~
+brew cask install git iterm2 zsh mysql google-chrome alfred dash dropbox qlmarkdown qlcolorcode mackup 
+
+# 软件配置还原，很重要啊
+mackup list
+mackup backup
+
