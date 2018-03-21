40832584bb9029ff11f92d19fa430b69d8b9a6d2
luzhangting <luzhangting@meituan.com>
2018-03-21 19:01:11 +0800

    add software

--- my_custom_mac.sh
+++ my_custom_mac.sh
@@ -2,7 +2,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # 使用brew安装软件，brew cask能一次安装好多软件~~
-brew cask install git iterm2 zsh mysql google-chrome alfred dash dropbox qlmarkdown qlcolorcode mackup 
+brew cask install git iterm2 zsh mysql google-chrome alfred dash dropbox qlmarkdown qlcolorcode mackup anaconda 
 
 # 软件配置还原，很重要啊
 mackup list
